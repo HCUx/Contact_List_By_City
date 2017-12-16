661dc28a0bc8b588f54617aca25c4b1e4613c794
Hamit <canuzunay@gmail.com>
2017-12-16 19:28:53 +0300

    add Comments

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
 <html>
 <head><title>Hamit Can Uzunay - Odev 2</title>
 <style>
+    <!-- Tablo etrafına çerçeve için alttaki css kodu -->
     table, th, td {
         border: 2px solid #ffcb67;
     }
 </style></head>
 <body>
 <?php
-$username = "root";
-$password = "";
-$databasename = "odev2";
+$username = "root"; //database kullanıcı adı
+$password = "";     //database şifresi - bende şifre olmadığı için boş geçtim
+$databasename = "odev2"; //bağlanmak istediğimiz database adı
 
+/*
+ * alttaki $connection nesnesi bizim bağlantı bilgilerini tutmakta
+ * bağlanmak için mysqli_connect(host,username,password,databaseName);
+ */
 $connection = mysqli_connect("localhost",$username,$password,$databasename);
 
+//herhangi bir bağlanma hatası oldu mu kontrol ediyoruz
 if ($connection->connect_error)
     die("Connection failed: " . $conn->connect_error);
 
-
-
+/*
+ * $queryString değişkeni string olarak sorgu cümlemizi tutmakta bunu server'a string olarak göndeririz
+ * o kendisi yorumlar yanlış ise bize hata dönderir
+ */
 $queryString = "SELECT * FROM odev2.sehir";
+
+/*
+ * $resultCityTable değişkeni kendisine gönderilen sorgu cümlesinin sonucu tutar
+ * biz select ile veri çektiğimiz için içerisinde tablo verileri olacaktır
+ */
 $resultCityTable = $connection->query($queryString);
 
 if ($resultCityTable->num_rows > 0) {
@@ -27,7 +40,7 @@ if ($resultCityTable->num_rows > 0) {
         echo "<tr>";
         echo "<td>". $row["ID"] ."</td>";
         echo "<td>". $row["SEHIR_AD"] ."</td>";
-        echo "<td><a href='index.php?city_id=".$row["ID"]."'>Bana Tıkla</a> </td>";
+        echo "<td><a href='index.php?city_id=".$row["ID"]."'>Bana Tıkla</a> </td>"; //click yapılacak linkin uzantısına şehir plaka kodu ekledik
         echo "</tr>";
     }
     echo "</table>";
